798bae2e216ad8965a679ee131087d10560bf3c3
AnastasiyaPolyanskaya <anastasiya-perep@mail.ru>
2025-12-05 14:16:25 +0300

    Create task_2_average_value.py

---
+++ python-practice/python_strings_and_fstring/task_2_average_value.py
@@ -0,0 +1,12 @@
+# Задача 2 — Нахождение среднего значения списка
+
+numbers = [5, 12, 8, 3, 27]
+
+total = sum(numbers)
+count = len(numbers)
+average = total / count
+
+print("Список чисел:", numbers)
+print("Сумма элементов:", total)
+print("Количество элементов:", count)
+print("Среднее значение:", average)
