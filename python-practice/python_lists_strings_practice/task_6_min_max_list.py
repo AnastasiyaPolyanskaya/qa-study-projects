608ca305a7c8dcd586655f8a5746470f71f75a2d
AnastasiyaPolyanskaya <anastasiya-perep@mail.ru>
2025-12-05 14:23:34 +0300

    Create task_6_min_max_list.py

---
+++ python-practice/python_lists_strings_practice/task_6_min_max_list.py
@@ -0,0 +1,10 @@
+# Задача 6 - Минимум и максимум списка
+
+numbers = [14, 2, 88, 23, 9, 100, 7]
+
+minimum = min(numbers)
+maximum = max(numbers)
+
+print("Список:", numbers)
+print("Минимум:", minimum)
+print("Максимум:", maximum)
