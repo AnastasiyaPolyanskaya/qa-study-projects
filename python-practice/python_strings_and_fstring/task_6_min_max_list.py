8c16e492b543fe82c3a2fd46cecb45bc3f588e8f
AnastasiyaPolyanskaya <anastasiya-perep@mail.ru>
2025-12-05 14:19:10 +0300

    Create task_6_min_max_list.py

---
+++ python-practice/python_strings_and_fstring/task_6_min_max_list.py
@@ -0,0 +1,10 @@
+# Задача 6 - Нахождение минимального и максимального значения в списке
+
+numbers = [14, 3, 67, 22, 1, 90, 11]
+
+minimum = min(numbers)
+maximum = max(numbers)
+
+print("Список чисел:", numbers)
+print("Минимальное значение:", minimum)
+print("Максимальное значение:", maximum)
