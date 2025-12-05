7e1a1e530c60fff9586df6916ede28897753b539
AnastasiyaPolyanskaya <anastasiya-perep@mail.ru>
2025-12-05 11:20:58 +0300

    Create task_5_replace_word.py

---
+++ python-practice/python_string_practice/task_5_replace_word.py
@@ -0,0 +1,8 @@
+text = input("Введите строку: ")
+old = input("Введите слово, которое нужно заменить: ")
+new = input("Введите новое слово: ")
+
+# Замена слова
+result = text.replace(old, new)
+
+print("Результат:", result)
